0df06f2748dc68137ae93411dc00ee85377a96e0
Raja Muhammad Ahsan <ahsanrajaq056@gmail.com>
2026-03-19 16:17:26 +0500

    Update test_predict.py

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -2,17 +2,18 @@ from fastapi.testclient import TestClient
 from src.api.app import app
 
 def test_predict():
-    payload = {
-        "hour": 14,
-        "day": 10,
-        "month": 3,
-        "day_of_week": 2
-    }
+    with TestClient(app) as client:   #  ensures startup event runs
+        payload = {
+            "hour": 14,
+            "day": 10,
+            "month": 3,
+            "day_of_week": 2
+        }
 
         response = client.post("/predict", json=payload)
 
         print("Response JSON:", response.json())
 
-    assert response.status_code == 200
-    data = response.json()
-    assert "prediction" in data
+        assert response.status_code == 200
+        data = response.json()
+        assert "predicted_global_active_power" in data
